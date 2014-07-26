fb5356ccd408cca3348e66d211500623078700aa
Jonathan Salfati <jonathan.salfati@rodanotech.ch>
2014-07-27 00:19:41 +0200

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,76 @@
+# This script aims at providing tidy data for the Human Activity Recognition dataset.
+# Data coming from http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+# Dataset is the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors.
+
+# To provide tidy data the script does the following
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names. 
+#Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+# Read files
+
+
+dataset_root_dir <- "UCI\ HAR\ Dataset"
+folders <- c("test","train")
+
+toto <- c("body_acc", "body_gyro", "total_acc")
+
+inertial_signals_folder <- "Inertial Signals"
+
+body.gyro <- data.frame()
+body.acc <- data.frame()
+total.acc <- data.frame()
+subjects <- data.frame()
+experiences <- data.frame()
+labels <- data.frame()
+
+# Read train and test dataset
+# Add data in data frame, based on file name pattern 
+# 		body.gyro
+# 		body.acc
+# 		total.acc
+# 		subjects
+# 		experiences
+# 		labels
+
+setwd(dataset_root_dir)
+files <- (list.files(".", include.dirs = T, recursive=T))
+lapply(files, function(x){
+	if (grepl("body_acc_", x)){
+		df <- read.table(file=x)
+		body.acc <<- rbind (body.acc, df)
+		print (paste(x, " imported"))
+	}
+	else if (grepl("body_gyro_", x)){
+		df <- read.table(file=x)
+		body.gyro <<- rbind (body.gyro, df)
+		print (paste(x, " imported"))
+	}
+	else if (grepl("total_acc_", x)){
+		df <- read.table(file=x)
+		total.acc <<- rbind (total.acc, df)
+		print (paste(x, " imported"))
+	}
+	else if (grepl("subject_", x)){
+		df <- read.table(file=x)
+		subjects <<- rbind (subjects, df)
+		print (paste(x, " imported"))
+	}
+	else if (grepl("X_", x)){
+		df <- read.table(file=x)
+		experiences <<- rbind (experiences, df)
+		print (paste(x, " imported"))
+	}
+	else if (grepl("y_", x) &  !grepl("activity_labels.txt", x)){
+		df <- read.table(file=x)
+		labels <<- rbind (labels, df)
+		print (paste(x, " imported"))
+	}
+	else {
+		print (paste(x, " not imported"))
+	}
+})
+setwd("..")
